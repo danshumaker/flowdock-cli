84eddecdb61f5415dc59c9a6ed40c0f8fc85e197
Dan Shumaker <dshumaker@phase2technology.com>
2019-05-02 16:22:48 -0400

    Config updates

--- bin/flowdock.js
+++ bin/flowdock.js
@@ -34,7 +34,6 @@ requirejs([
         .command('config')
         .description('Change configuration')
         .option('-c, --clear', 'Clear stored configuration')
-        .option('-t, --template <template>', 'Start config with this given template', String)
         .option('-v, --verbose', 'verbose debugging output')
         .action(function(options) {
             if (options.clear) {
@@ -45,18 +44,16 @@ requirejs([
         }).on('--help', function() {
             console.log('  Config Help:');
             console.log();
-            console.log('    Jira URL: https://foo.atlassian.net/');
+            console.log('    Flowdock sub URL: company/flow -NOTE:Do not include protocol and flowdock.com url');
             console.log('    Username: user (for user@foo.bar)');
             console.log('    Password: Your password');
             console.log('');
-            console.log('WARNING:After three failed login attempts Atlassian forces a CAPTCHA login');
-            console.log('WARNING:  which can only be done via the browser.');
         });
 
     program.parse(process.argv);
 
     if (program.args.length === 0) {
-        console.log("\nYour first step is to run the config option.\n");
+        console.log("\nYour first step is to run `flowdock config`.\n");
         program.help();
     }
 
